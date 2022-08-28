4137e936a8a4c01d248cdd6c1a13fcfca5346bd4
nithinpaulraj <paulnithin137@gmail.com>
2022-08-28 20:56:03 +0530

    day

---
+++ src/org/basic/Task.java
@@ -0,0 +1,14 @@
+package org.basic;
+
+public class Task {
+	int id=10;
+	public void m1() {
+		System.out.println(id);
+	
+	}
+	public static void main(String[] args) {
+		Task t=new Task();
+		t.m1();
+	}
+
+}
